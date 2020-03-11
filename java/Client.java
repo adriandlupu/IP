f1e72888c55ccd44d5c541349e98143f62449503
RDaniel99 <rusudaniel5799@gmail.com>
2020-03-11 09:23:34 +0200

    Add files via upload
    reverse

---
+++ java/Client.java
@@ -0,0 +1,29 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package com.company;
+
+import java.util.Vector;
+
+public class Client
+{
+    public String Mail;
+    public Vector myPlanta;
+    public Admin myAdmin;
+    
+    public void scanarePlanta() {
+    }
+    
+    public void scanarePlacuta() {
+    }
+    
+    public void cautareDenumire() {
+    }
+    
+    public void salvarePreferate() {
+    }
+    
+    public void vizulalizarePlanta() {
+    }
+}
