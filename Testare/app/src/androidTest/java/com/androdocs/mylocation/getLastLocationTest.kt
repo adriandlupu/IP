04550282162040668970cef7155defdda38dc7b3
madabitiusca <61908306+madabitiusca@users.noreply.github.com>
2020-05-06 08:52:27 +0300

    .

---
+++ Testare/app/src/androidTest/java/com/androdocs/mylocation/getLastLocationTest.kt
@@ -0,0 +1,26 @@
+package com.androdocs.mylocation
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class getLastLocationTest :() {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun testCheckPermission() {
+    }
+
+    @Test
+    fun testGetMFusedLocationClient() {
+    }
+}
